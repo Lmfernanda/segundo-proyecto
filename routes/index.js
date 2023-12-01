04b0127a6b782d8af5986a316c2ffdeb5dbf4b5b
Lmfernanda <mflopezs@correo.udistrital.edu.co>
2023-11-30 20:56:34 -0500

    :recycle: refactor in code

--- routes/index.js
+++ routes/index.js
@@ -9,106 +9,68 @@ router.get('/', function (req, res, next) {
 
 //Enrutamiento para visualizar los medicos de la base de datos
 router.get('/listado-medicos', (req, res) => {
-  conexion.query('SELECT * FROM medicos;', (error, resultado) => {
-    if (error) {
-      console.log('Ocurrio un error en la ejecucion', error)
-      res.status(500).send('Error en la ejecucion')
-    } else {
-      console.log(resultado)
-      res.status(200).render('medicos', { resultado })
-    }
-  })
+  conexion.query('SELECT * FROM medicos;', (error, resultado) => handleErrorListing(error, resultado, undefined, undefined, 'medicos', res))
 })
 
 //Enrutamiento para agregar un medico a la base de datos
 router.post('/agregar-medico', (req, res) => {
-  const nombres = req.body.nombres
-  const apellidos = req.body.apellidos
-  const cedula = req.body.cedula
-  const consultorio = req.body.consultorio
-  const telefono = req.body.telefono
-  const correo = req.body.correo
-  const especialidad = req.body.especialidad
+  const {nombres, apellidos, cedula, consultorio, telefono, correo, especialidad} = req.body
 
-  conexion.query(`INSERT INTO medicos (cedula, nombres, apellidos, especialidad, consultorio, correo, telefono) VALUES (${cedula}, '${nombres}', '${apellidos}', '${especialidad}', '${consultorio}', '${correo}', '${telefono}')`, (error, resultado) => {
-    if (error) {
-      console.log('Ocurrio un error en la ejecucion', error)
-      res.status(500).send('Error en la ejecucion')
-    } else {
-      res.status(200).redirect('listado-medicos')
-    }
-  })
+  conexion.query(`INSERT INTO medicos (cedula, nombres, apellidos, especialidad, consultorio, correo, telefono) VALUES (${cedula}, '${nombres}', '${apellidos}', '${especialidad}', '${consultorio}', '${correo}', '${telefono}')`, (error) => handleErrorSaving(error, undefined, undefined, 'listado-medicos', res))
 });
 
 //Enrutamiento para visualizar los pacientes de la base de datos
 router.get('/listado-pacientes', (req, res) => {
-  conexion.query('SELECT * FROM pacientes;', (error, resultado) => {
-    if (error) {
-      console.log('Ocurrio un error en la ejecucion', error)
-      res.status(500).send('Error en la ejecucion')
-    } else {
-      console.log(resultado)
-      res.status(200).render('pacientes', { resultado })
-    }
-  })
+  conexion.query('SELECT * FROM pacientes;', (error, resultado) => handleErrorListing(error, resultado, undefined, undefined, 'pacientes', res))
 })
 
 //Enrutamiento para visualizar las citas agendadas
 router.get('/listado-citas', (req, res) => {
-  conexion.query('SELECT fecha_cita, pacientes.nombres, pacientes.apellidos, pacientes.telefono, medicos.especialidad, medicos.consultorio, medicos.nombres nombresMedico, medicos.apellidos apellidosMedico FROM cita_medica, pacientes, medicos WHERE cedula_medico=medicos.cedula AND cedula_paciente=pacientes.cedula;', (error, resultado) => {
-    if (error) {
-      console.log('Ocurrio un error en la ejecucion', error)
-      res.status(500).send('Error en la ejecucion')
-    } else {
-      res.status(200).render('citas', { resultado })
-    }
-  })
+  conexion.query('SELECT fecha_cita, pacientes.nombres, pacientes.apellidos, pacientes.telefono, medicos.especialidad, medicos.consultorio, medicos.nombres nombresMedico, medicos.apellidos apellidosMedico FROM cita_medica, pacientes, medicos WHERE cedula_medico=medicos.cedula AND cedula_paciente=pacientes.cedula;', (error, resultado) => handleErrorListing(error, resultado, undefined, undefined, 'citas', res))
 })
 
 //Enrutamiento para agregar un paciente a la base de datos
 router.post('/agregar-paciente', (req, res) => {
-  const nombres = req.body.nombres
-  const apellidos = req.body.apellidos
-  const cedula = req.body.cedula
-  const fecha_nacimiento = req.body.fecha_nacimiento
-  const telefono = req.body.telefono
+  const {nombres, apellidos, cedula, fecha_nacimiento, telefono} = req.body
 
-  conexion.query(`INSERT INTO pacientes (cedula, nombres, apellidos, fecha_nacimiento, telefono) VALUES (${cedula}, '${nombres}', '${apellidos}', '${fecha_nacimiento}','${telefono}')`, (error, resultado) => {
-    if (error) {
-      console.log('Ocurrio un error en la ejecucion', error)
-      res.status(500).send('Error en la ejecucion')
-    } else {
-      res.status(200).redirect('listado-pacientes')
-    }
-  })
+  conexion.query(`INSERT INTO pacientes (cedula, nombres, apellidos, fecha_nacimiento, telefono) VALUES (${cedula}, '${nombres}', '${apellidos}', '${fecha_nacimiento}','${telefono}')`, (error) => handleErrorSaving(error, undefined, undefined, 'listado-pacientes', res))
 });
 
-router.post('/consulta-cita', (req, res) => {
-  const especialidad = req.body.especialidad
-  conexion.query(`SELECT * FROM medicos WHERE especialidad='${especialidad}';`, (error, resultado) => {
-    if (error) {
-      console.log(error)
-      res.status(500).send('Ocurrio un error en la consulta')
-    } else {
-      console.log({ resultado })
-      res.status(200).render('agenda-citas', { resultado })
-    }
-  })
+router.get('/consulta-cita', (req, res) => {
+  
+  const especialidad = req.query.especialidad
+
+  conexion.query(`SELECT * FROM medicos WHERE especialidad='${especialidad}';`, (error, resultado) => handleErrorListing(error, resultado, undefined, undefined, 'agenda-citas', res))
 })
 
 router.post('/agregar-cita', (req, res) => {
-  const cedula_paciente = req.body.cedula
-  const fecha_cita = req.body.fecha_cita
-  const cedula_medico = req.body.especialista
+  const {cedula, fecha_cita, especialista} = req.body
+  
   console.log(req.body)
-  conexion.query(`INSERT INTO cita_medica (cedula_medico, cedula_paciente, fecha_cita) VALUES (${cedula_medico}, ${cedula_paciente}, '${fecha_cita}')`, (error, resultado) => {
-    if (error) {
-      console.log(error)
-      res.status(500).send('Ocurrio un error en la consulta')
-    } else {
-      res.status(200).redirect('/listado-citas')
-    }
-  })
+  conexion.query(`INSERT INTO cita_medica (cedula_medico, cedula_paciente, fecha_cita) VALUES (${especialista}, ${cedula}, '${fecha_cita}')`, (error) => handleErrorSaving(error, undefined, undefined, '/listado-citas', res))
 })
+
+const handleErrorListing = (
+  error, resultado, statusError= 500, statusRes= 200, view, response,
+) => {
+  if(error) {
+    console.log(error)
+    response.status(statusError).send('Ocurrio un error obteniendo la información de la base de datos')
+  } else {
+    console.log(resultado)
+    response.status(statusRes).render(view, { resultado })
+  }
+}
+
+const handleErrorSaving = (
+  error, statusError=500, statusRes=200, view, response,
+) => {
+  if(error) {
+    console.log(error)
+    response.status(statusError).send('Ocurrio un error guardando la información de la base de datos')
+  } else {
+    response.status(statusRes).redirect(view)
+  }
+}
 
 module.exports = router;
